6a2ebe3f7ff77383b3368c2b1b429b6ed29166e2
Viktor Pancheluga <v.pancheluga@adbakers.com.ua>
2018-09-02 18:59:37 +0300

    start cordoba

---
+++ my-project/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    fkflasfklasfklaskflkaslfklkasfklsfk
+  </div>
+</template>
+
+<script>
+import HomePage from './components/HomePage'
+import MenuPage from './components/MenuPage'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      menuIsOpen: false
+    }
+  },
+  components: {
+    HomePage,
+    MenuPage
+  },
+    mounted() {
+      alert(window.cordova);
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ons-splitter-side[side=left][animation=overlay] {
+  border-right: 1px solid #BBB;
+}
+</style>
